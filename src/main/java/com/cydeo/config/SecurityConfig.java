c3cb1b79861a0e533863ca062c5d1064717a6c77
MertRam <mert.ramadan35@gmail.com>
2023-11-02 16:53:45 +0300

    SecurityService.java and impl and UserPrincipal added

--- src/main/java/com/cydeo/config/SecurityConfig.java
+++ src/main/java/com/cydeo/config/SecurityConfig.java
@@ -18,25 +18,32 @@ import java.util.List;
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
-
-
-        List<UserDetails> userList = new ArrayList<>();
-        userList.add(
-                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
-        );
-        userList.add(new User("OZZY", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
-        return new InMemoryUserDetailsManager(userList);
-
-    }
+//    @Bean
+//    public UserDetailsService userDetailsService(PasswordEncoder encoder) {
+//
+//
+//        List<UserDetails> userList = new ArrayList<>();
+//        userList.add(
+//                new User("mike", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_ADMIN")))
+//        );
+//        userList.add(new User("OZZY", encoder.encode("password"), Arrays.asList(new SimpleGrantedAuthority("ROLE_MANAGER"))));
+//        return new InMemoryUserDetailsManager(userList);
+//
+//    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         return http
                 .authorizeRequests()
-                .antMatchers(
+//                .antMatchers("/user/**").hasRole("ADMIN")
+//                .antMatchers("/project/**").hasRole("MANAGER")
+//                .antMatchers("/task/employee/**").hasRole("EMPLOYEE")
+//                .antMatchers("/task/**").hasRole("MANAGER")
+                //.antMatchers("/task/**").hasAnyRole("EMPLOYEE","ADMIN")
+                //.antMatchers("/task/**").hasAuthority("ROLE_EMPLOYEE")
+                .antMatchers("/user/**").hasAuthority("ROLE_ADMIN")
+                                .antMatchers(
                         "/",
                         "/login",
                         "/fragments/**",
@@ -45,7 +52,12 @@ public class SecurityConfig {
                 ).permitAll()
                 .anyRequest().authenticated()
                 .and()
-                .httpBasic()
+ //               .httpBasic()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/welcome")
+                .failureUrl("/login?error=true")
+                .permitAll()
                 .and().build();
 
     }
